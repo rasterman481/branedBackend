795d362dd9d3a2ef599bae760287b30c7aa3bf95
rasterman481 <rasterman481@gmail.com>
2024-07-23 18:26:33 +0530

    Base change for DB from mongo to PostGres

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const connectDb = require("./db/connectDb");
+const {connectDb} = require("./db/connectDb");
 const userRouter = require("./routes/userRouter");
 const domainRouter = require("./routes/domainRouter");
 const linkRouter = require("./routes/linkRouter");
@@ -23,15 +23,40 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
+  console.log("Base route hit");
   res.send("Api is running");
 });
 
-app.use("/api/user", userRouter);
-app.use("/api/domain", domainRouter);
-app.use("/api/links", linkRouter);
-app.use("/api/subdomain", subDomainRouter);
-app.post("/api/forward", forwardLink);
-app.use("/api/payment", paymentRouter);
+app.use("/api/user", (req, res, next) => {
+  console.log("User route hit");
+  next();
+}, userRouter);
+
+app.use("/api/domain", (req, res, next) => {
+  console.log("Domain route hit");
+  next();
+}, domainRouter);
+
+app.use("/api/links", (req, res, next) => {
+  console.log("Links route hit");
+  next();
+}, linkRouter);
+
+app.use("/api/subdomain", (req, res, next) => {
+  console.log("Subdomain route hit");
+  next();
+}, subDomainRouter);
+
+app.post("/api/forward", (req, res, next) => {
+  console.log("Forward link route hit");
+  next();
+}, forwardLink);
+
+app.use("/api/payment", (req, res, next) => {
+  console.log("Payment route hit");
+  next();
+}, paymentRouter);
+
 
 // const options = {
 //   key: fs.readFileSync(path.join(__dirname, "lib", "SSL", "key.pem")),
@@ -39,10 +64,12 @@ app.use("/api/payment", paymentRouter);
 // };
 
 // const server = https.createServer(options, app);
+const User = require("./model/userSchema.js");
 
 (async () => {
   try {
-    await connectDb(process.env.DB_URI);
+    // await connectDb(process.env.DB_URI);
+    await connectDb();
     app.listen(PORT, () => console.log(`Server is running on PORT:${PORT}`));
   } catch (error) {
     console.log(error);
--- db/connectDb.js
+++ db/connectDb.js
@@ -1,10 +1,44 @@
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+// const mongoose = require("mongoose");
+// mongoose.set("strictQuery", false);
 
-const connectDb = (uri) =>
-  mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(console.log("Db connected"))
-    .catch((error) => console.log(error));
+// const connectDb = (uri) =>
+//   mongoose
+//     .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(console.log("Db connected"))
+//     .catch((error) => console.log(error));
+// module.exports = { connectDb} ;
 
-module.exports = connectDb;
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: process.env.DB_PORT,
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 20,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+});
+
+const connectDb = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log('PostgreSQL connected');
+  } catch (error) {
+    console.error('PostgreSQL connection error:', error);
+    process.exit(1);
+  }
+};
+
+module.exports = { sequelize, connectDb };
